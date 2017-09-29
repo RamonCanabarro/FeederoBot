b36ef4af4897a7feeb5c07517d73289c43c41572
ramoncanabarro <ramoncanabarro@gmail.com>
2017-09-29 14:04:42 -0300

    atualização do bot e banco

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sys
 import time
 import telepot
@@ -5,28 +6,41 @@ import feedparser
 from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def on_chat_message(msg):
+    conn=sqlite3.connect('feed.db')
+
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     noticia = feedparser.parse('http://www.criptomoedasfacil.com/feed/')
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Clique aquipara mais', callback_data='press')],
+                   [InlineKeyboardButton(text='Clique aqui para mais', callback_data='press')],
                ])
 
     bot.sendMessage(chat_id, 'Novas notícias',reply_markup=keyboard )
-   
-
-def on_callback_query(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-
-    noticia = feedparser.parse('http://www.criptomoedasfacil.com/feed/')
-      
     for post in range(3):
       bot.sendMessage(chat_id, noticia.entries[post].title + ": " + noticia.entries[post].link + "")
 
+
+    params =  (noticia['feed']['title'],noticia.entries[0].title, noticia['feed']['link'], chat_id, 0)
+    sql = ''' INSERT INTO feed(titulo,descricao,link,user, is_first_post)
+              VALUES(?,?,?,?,?)'''
+    curs = conn.cursor()
+    curs.execute(sql,params)
+    conn.commit()
+    conn.close()
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='chat')
+    print('Callback Query:', query_id, from_id, query_data)
+
+    noticia = feedparser.parse('http://www.criptomoedasfacil.com/feed/')
+    print('Callback Query:', query_id, from_id, query_data)
+    bot.answerCallbackQuery(query_id, text=noticia['feed']['title'])
     
-TOKEN = TOOOOKEN  # get token from command-line
+    
+TOKEN = ''  # get token from command-line
 
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat': on_chat_message,
