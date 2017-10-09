d3d80044968d0c322083a51dcc5aac7f0e220a3b
ramoncanabarro <ramoncanabarro@gmail.com>
2017-10-09 10:22:39 -0300

    Suspensão de desenvolvimento por ordem maior

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 import sys
 import time
 import telepot
@@ -7,25 +8,97 @@ from telepot.loop import MessageLoop
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+ 
+def on_list_command(msg):   
+        conn=sqlite3.connect('feed.db')
+        text, chat_type, chat_id = telepot.glance(msg)
+        bot = telepot.Bot(TOKEN)
+
+        sql = 'SELECT link FROM feed WHERE user = ?'
+        curs = conn.cursor()
+        id = (chat_id,)
+        curs.execute(sql,id)
+
+        exist = (curs.fetchall())
+
+        if(not exist):
+            bot.sendMessage('Não há registros.')
+        else:
+            cont = 0
+            for post in exist:
+                cont+=1            
+                bot.sendMessage(chat_id,  (str(cont)+ " ---- " + json.dumps(post)))
+
+
+def on_delete_command(msg):
+    conn = sqlite3.connect('feed.db')
+    text, chat_type, chat_id = telepot.glance(msg)
+    bot = telepot.Bot(TOKEN)
+    bot.sendMessage(chat_id, 'Selecione o feed enviando o número a ser removido.')
+    on_list_command(msg)
+    link = bot.getUpdates()
+
+    while (not link):
+        link = bot.getUpdates()
+
+    rmo =  (int(link[0]['message']['text'])-1)
+    sql = 'SELECT link FROM feed WHERE user = ?'
+
+    curs = conn.cursor()
+    id = (chat_id,)
+    curs.execute(sql, id)
+    exist = (curs.fetchall())    
+
+    curs = conn.cursor()
+    sql= 'DELETE FROM feed WHERE user = ? AND link = ?'
+    params  = (chat_id,exist[rmo][0])
+    print (str(exist[rmo]))
+    print (params)
+    curs.execute(sql, params)
+    conn.commit()
+    conn.close()
+
 def on_chat_message(msg):
     conn=sqlite3.connect('feed.db')
+    text, chat_type, chat_id = telepot.glance(msg)
+    bot = telepot.Bot(TOKEN)
+    link = bot.getUpdates()
+    feed =  (link[0]['message']['text'])
 
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    # READS AND REDIRECTS ALL COMMANDS GIVEN --------------------------------------------------------
+    if ('/' in  feed):
+        mensagem = (feed[1:8])        
+        if(mensagem == 'listar'):
+            on_list_command(msg)
+        elif(mensagem == 'remover'):    
+            on_delete_command(msg)    
 
-    noticia = feedparser.parse('http://www.criptomoedasfacil.com/feed/')
+    noticia = feedparser.parse(feed)
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
                    [InlineKeyboardButton(text='Clique aqui para mais', callback_data='press')],
                ])
+    sql = 'SELECT user,link FROM feed WHERE user = ?'
+    curs = conn.cursor()
+    id = (chat_id,)
+    curs.execute(sql, id)
+    exist = (curs.fetchone()) #Futuramente receber array de resultados
 
-    bot.sendMessage(chat_id, 'Novas notícias',reply_markup=keyboard )
-    for post in range(3):
-      bot.sendMessage(chat_id, noticia.entries[post].title + ": " + noticia.entries[post].link + "")
+    if ((not exist) or ((exist[0] == str(chat_id)) and (exist[1] != feed) )):
+      for post in range(3):
+        bot.sendMessage(chat_id, noticia.entries[post].title + ": " + noticia.href + "")    
+      sql = ''' INSERT INTO feed(titulo,descricao,link,user) VALUES(?,?,?,?)'''
+    else:
+        
+        bot.sendMessage(chat_id, noticia.entries[0].title + ": " + noticia.entries[0].link + "")
+        sql='''UPDATE feed set titulo=?, descricao=?, link=? where user=?'''
+    
+    print ('--------------------------------------------------------------------------------------')
+    print (noticia['href'])
+    params =  (noticia['href'],noticia.entries[0].title, noticia['href'], chat_id)   
 
 
-    params =  (noticia['feed']['title'],noticia.entries[0].title, noticia['feed']['link'], chat_id, 0)
-    sql = ''' INSERT INTO feed(titulo,descricao,link,user, is_first_post)
-              VALUES(?,?,?,?,?)'''
+
     curs = conn.cursor()
     curs.execute(sql,params)
     conn.commit()
@@ -40,7 +113,7 @@ def on_callback_query(msg):
     bot.answerCallbackQuery(query_id, text=noticia['feed']['title'])
     
     
-TOKEN = ''  # get token from command-line
+TOKEN = 'TOKEN'  # get token from command-line
 
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat': on_chat_message,
